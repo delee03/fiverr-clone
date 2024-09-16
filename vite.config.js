649ee7fa1409c461be31b99bc9672745dd1f54c1
Pham Phat <phamphat2003vn@gmail.com>
2024-09-16 21:41:28 +0700

    ....

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 1000, // Set the limit higher than 500 kB
+    },
+});
