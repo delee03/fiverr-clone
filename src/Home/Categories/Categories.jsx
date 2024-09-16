23749460653f41f6f82ae76c5c59d8d84412f5af
Pham Phat <phamphat2003vn@gmail.com>
2024-09-16 22:06:08 +0700

    Refactor category and hero component styles

--- src/Home/Categories/Categories.jsx
+++ src/Home/Categories/Categories.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "./Categories.scss";
-import "./ResCategories.scss";
 
 import Categories_fiverr1 from "../../assets/images/Categories_fiverr1.svg";
 import Categories_fiverr2 from "../../assets/images/Categories_fiverr2.svg";
@@ -12,75 +10,77 @@ import Categories_fiverr7 from "../../assets/images/Categories_fiverr7.svg";
 import Categories_fiverr8 from "../../assets/images/Categories_fiverr8.svg";
 import Categories_fiverr9 from "../../assets/images/Categories_fiverr9.svg";
 
+import "./categories.scss";
+
 const Categories_Img = [
-  {
-    href: "https://www.fiverr.com/categories/programming-tech?source=hplo_cat_sec_TOP&pos=1",
-    title: "Programing & Tech",
-    image: Categories_fiverr1,
-  },
-  {
-    href: "https://www.fiverr.com/categories/graphics-design?source=hplo_cat_sec_TOP&pos=2",
-    title: "Graphics & Design",
-    image: Categories_fiverr2,
-  },
-  {
-    href: "https://www.fiverr.com/categories/online-marketing?source=hplo_cat_sec_TOP&pos=3",
-    title: "Digital & Marketing",
-    image: Categories_fiverr3,
-  },
-  {
-    href: "https://www.fiverr.com/categories/writing-translation?source=hplo_cat_sec_TOP&pos=4",
-    title: "Writing & Translation",
-    image: Categories_fiverr4,
-  },
-  {
-    href: "https://www.fiverr.com/categories/video-animation?source=hplo_cat_sec_TOP&pos=5",
-    title: "Video & Animation",
-    image: Categories_fiverr5,
-  },
-  {
-    href: "https://www.fiverr.com/categories/ai-services?source=hplo_cat_sec_TOP&pos=6",
-    title: "AI Services",
-    image: Categories_fiverr6,
-  },
-  {
-    href: "https://www.fiverr.com/categories/music-audio?source=hplo_cat_sec_TOP&pos=7",
-    title: "Music & Audio",
-    image: Categories_fiverr7,
-  },
-  {
-    href: "https://www.fiverr.com/categories/business?source=hplo_cat_sec_TOP&pos=8",
-    title: "Bussiness",
-    image: Categories_fiverr8,
-  },
-  {
-    href: "https://www.fiverr.com/categories/consulting-services?source=hplo_cat_sec_TOP&pos=9",
-    title: "Consulting",
-    image: Categories_fiverr9,
-  },
+    {
+        href: "https://www.fiverr.com/categories/programming-tech?source=hplo_cat_sec_TOP&pos=1",
+        title: "Programing & Tech",
+        image: Categories_fiverr1,
+    },
+    {
+        href: "https://www.fiverr.com/categories/graphics-design?source=hplo_cat_sec_TOP&pos=2",
+        title: "Graphics & Design",
+        image: Categories_fiverr2,
+    },
+    {
+        href: "https://www.fiverr.com/categories/online-marketing?source=hplo_cat_sec_TOP&pos=3",
+        title: "Digital & Marketing",
+        image: Categories_fiverr3,
+    },
+    {
+        href: "https://www.fiverr.com/categories/writing-translation?source=hplo_cat_sec_TOP&pos=4",
+        title: "Writing & Translation",
+        image: Categories_fiverr4,
+    },
+    {
+        href: "https://www.fiverr.com/categories/video-animation?source=hplo_cat_sec_TOP&pos=5",
+        title: "Video & Animation",
+        image: Categories_fiverr5,
+    },
+    {
+        href: "https://www.fiverr.com/categories/ai-services?source=hplo_cat_sec_TOP&pos=6",
+        title: "AI Services",
+        image: Categories_fiverr6,
+    },
+    {
+        href: "https://www.fiverr.com/categories/music-audio?source=hplo_cat_sec_TOP&pos=7",
+        title: "Music & Audio",
+        image: Categories_fiverr7,
+    },
+    {
+        href: "https://www.fiverr.com/categories/business?source=hplo_cat_sec_TOP&pos=8",
+        title: "Bussiness",
+        image: Categories_fiverr8,
+    },
+    {
+        href: "https://www.fiverr.com/categories/consulting-services?source=hplo_cat_sec_TOP&pos=9",
+        title: "Consulting",
+        image: Categories_fiverr9,
+    },
 ];
 
 const Categories = () => {
-  return (
-    <div className="categories_section">
-      <div className="container">
-        <div className="categories">
-          <div className="categories_skill">
-            {Categories_Img.map((data, index) => (
-              <div className="categories_list">
-                <a href={data.href}>
-                  <div>
-                    <img src={data.image} alt="" />
-                  </div>
-                  <p>{data.title}</p>
-                </a>
-              </div>
-            ))}
-          </div>
+    return (
+        <div className="categories_section">
+            <div className="container">
+                <div className="categories">
+                    <div className="categories_skill">
+                        {Categories_Img.map((data, index) => (
+                            <div className="categories_list">
+                                <a href={data.href}>
+                                    <div>
+                                        <img src={data.image} alt="" />
+                                    </div>
+                                    <p>{data.title}</p>
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Categories;
--- src/Home/Hero/Hero.jsx
+++ src/Home/Hero/Hero.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import "./Hero.scss";
-import "./ResHero.scss";
+
 import IconSearch from "../../components/Icon/IconSearch";
 import ImageFiver1 from "../../assets/images/image_fiverr1.svg";
 import ImageFiver2 from "../../assets/images/image_fiverr2.svg";
@@ -9,73 +8,79 @@ import ImageFiver4 from "../../assets/images/image_fiverr4.svg";
 import ImageFiver5 from "../../assets/images/image_fiverr5.svg";
 import ImageFiver6 from "../../assets/images/image_fiverr6.svg";
 
+import "./Hero.scss";
+
 const imgHero = [
-  {
-    id: 1,
-    image: ImageFiver1,
-  },
-  {
-    id: 2,
-    image: ImageFiver2,
-  },
-  {
-    id: 3,
-    image: ImageFiver3,
-  },
-  {
-    id: 4,
-    image: ImageFiver4,
-  },
-  {
-    id: 5,
-    image: ImageFiver5,
-  },
-  {
-    id: 6,
-    image: ImageFiver6,
-  },
+    {
+        id: 1,
+        image: ImageFiver1,
+    },
+    {
+        id: 2,
+        image: ImageFiver2,
+    },
+    {
+        id: 3,
+        image: ImageFiver3,
+    },
+    {
+        id: 4,
+        image: ImageFiver4,
+    },
+    {
+        id: 5,
+        image: ImageFiver5,
+    },
+    {
+        id: 6,
+        image: ImageFiver6,
+    },
 ];
 
 const Hero = () => {
-  return (
-    <div className="hero_section pb-5">
-      <div className="container">
-        <div className="img_hero">
-          <div className="title_hero">
-            <h1 className="">
-              Find the right
-              <em className="title_freelancer"> freelancer </em> <br />
-              service, right away
-            </h1>
-            <div className="search_hero">
-              <form className="search_form" action="">
-                <input
-                  type="search"
-                  className="input_hero"
-                  placeholder="Search for any service..."
-                />
-                <button className="button_hero">
-                  <div className="button_icon">
-                    <IconSearch className="" />
-                  </div>
-                </button>
-              </form>
+    return (
+        <div className="hero_section pb-5">
+            <div className="container">
+                <div className="img_hero">
+                    <div className="title_hero">
+                        <h1 className="">
+                            Find the right
+                            <em className="title_freelancer">
+                                {" "}
+                                freelancer{" "}
+                            </em>{" "}
+                            <br />
+                            service, right away
+                        </h1>
+                        <div className="search_hero">
+                            <form className="search_form" action="">
+                                <input
+                                    type="search"
+                                    className="input_hero"
+                                    placeholder="Search for any service..."
+                                />
+                                <button className="button_hero">
+                                    <div className="button_icon">
+                                        <IconSearch className="" />
+                                    </div>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="trust_hero">
+                        <span>Trusted by:</span>
+                        <ul className="trust_image">
+                            {imgHero.map((data, index) => (
+                                <li>
+                                    <img src={data.image} alt="" />
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="trust_hero">
-            <span>Trusted by:</span>
-            <ul className="trust_image">
-              {imgHero.map((data, index) => (
-                <li>
-                  <img src={data.image} alt="" />
-                </li>
-              ))}
-            </ul>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Hero;
